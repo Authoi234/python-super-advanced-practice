10b1fd0acd9665c7e97123c6b99da0139ca89c69
Authoi234 <jawadkhan.authoi@gmail.com>
2025-09-25 19:10:44 +0600

    mixin doe

---
+++ mixin.py
@@ -0,0 +1,17 @@
+class Car:
+    def __init__ (self, name, manufacturer, price):
+        self.name = name
+        self.manufacturer = manufacturer
+        self.price = price
+
+class CarPriceMixin:
+    def clearance(self):
+        return self.price * 0.7
+    
+class ShowRoomCar(Car, CarPriceMixin):
+    pass
+
+if __name__ == "__main__":
+    c = ShowRoomCar("Camry", "Toyota", 38000)
+    print("Clearance price:", c.clearance())
+    
