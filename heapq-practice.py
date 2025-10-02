8527cfabd372423611c38229e043d69087a76eb4
Authoi234 <jawadkhan.authoi@gmail.com>
2025-10-02 16:37:06 +0600

    max heap class created using prebuilt heapq method

--- heapq-practice.py
+++ heapq-practice.py
@@ -23,3 +23,33 @@ def findKthLargest(nums: List[int], k:int) -> int:
         heapq.heappop(h)
 
     return h[0] * -1
+
+class MaxHeap:
+    def __init__(self, items=[]):
+        self.h = [x*-1 for  x in items]        
+        if items:
+            heapq.heapify(self.h)
+
+    def __len__(self):
+        return len(self.h)
+    
+    def push(self, item):
+        heapq.heappush(self.h,  -1*item)
+
+    def pop(self):
+        return -heapq.heappop(self.h)
+
+    def look(self):
+        return -1*self.h[0]
+    
+if __name__ == "__main__":
+    items=[1,3,5,7,9,2,4,6,8,0]
+    mx_heap = MaxHeap()
+    for item in items:
+        mx_heap.push(item)
+
+    print(mx_heap.look())
+
+    li = [mx_heap.pop() for _ in range(len(mx_heap))]
+
+    print(li)
